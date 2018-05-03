903264c26e9db934ede29d7ac59858722ed295f6
Altab Hossain <altab_ru@yahoo.com>
2018-05-03 18:29:19 -0400

    Initial commit

--- ConnectDb/Form1.cs
+++ ConnectDb/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ConnectDb
 {
@@ -15,6 +16,38 @@ namespace ConnectDb
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string connectionString = "Data Source=WIN-RTEN43736N0\\SQLEXPRESS;Initial Catalog=TestDb;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            /*
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM Emp WHERE ID = '" + textBox1.Text + "'", conn );
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            textBox1.Text = dt.Rows[0][0].ToString();
+            textBox2.Text = dt.Rows[0][1].ToString();
+            textBox3.Text = dt.Rows[0][2].ToString();
+            textBox4.Text = dt.Rows[0][3].ToString();
+            */
+
+            conn.Open();
+            //SqlCommand command = new SqlCommand("SELECT * FROM Emp WHERE ID = @id", conn);
+            //command.Parameters.Add(new SqlParameter("id", 1));
+            SqlCommand command = new SqlCommand("SELECT * FROM Emp", conn);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0} \t {1} \t {2} \t {3}", reader[0], reader[1], reader[2], reader[3])) ;
+                }
+            }
+
         }
     }
 }
